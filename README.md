7b36643e9dabb7a226d24051cc46a92a157ed7a0
ep-infosec <ep-infosec@users.noreply.github.com>
2022-12-14 11:01:50 +0200

    init

---
+++ README.md
@@ -0,0 +1,20 @@
+# waze-feed-archive
+
+This repository houses Python ETL script for scrapting Waze feed from waze API endpoint.
+
+A individual feed may looks like the example below.
+
+```
+{}"country":"US","nthumbsup":0,"city":"Austin, TX","reportrating":2,"confidence":0,"reliability":5,"type":"JAM","uuid":"","magvar":86,"subtype":"JAM_HEAVY_TRAFFIC","street":"N FM 620","reportdescription":null,"pubmillis":"","longitude":-97.775,"latitude":30.473734,"roadtype":7}
+```
+
+More details regards waze feed can be found at the link below:
+
+https://developers.google.com/waze/data-feed/overview
+
+## License
+
+As a work of the City of Austin, this project is in the public domain within the United States.
+
+Additionally, we waive copyright and related rights of the work worldwide through the [CC0 1.0 Universal public domain dedication](https://creativecommons.org/publicdomain/zero/1.0/).
+
---
+++ waze_psql.py
@@ -0,0 +1,111 @@
+"""Summary
+
+Attributes:
+    pgrest (TYPE): Description
+    waze_endpoint (TYPE): Description
+"""
+import pandas as pd
+import requests
+import json
+
+from tdutils.pgrestutil import Postgrest
+from pypgrest import Postgrest
+
+from config.secrets import *
+
+waze_endpoint = waze_endpoint
+
+
+pgrest = Postgrest(
+    POSTGRE_ENDPOINT,
+    auth=JOB_DB_API_TOKEN
+)
+
+
+def scrape_waze_record():
+    """Summary
+    
+    Returns:
+        TYPE: Description
+    """
+    # download json
+
+    r = requests.get(waze_endpoint)
+    alerts_list_raw = r.json()["alerts"]
+    # pd.DataFrame.from_dict(alerts_list[0])
+    return alerts_list_raw
+
+
+def split_coord(alerts_list_raw):
+    """Summary
+    
+    Args:
+        alerts_list_raw (TYPE): Description
+    
+    Returns:
+        TYPE: Description
+    """
+    alerts_list = alerts_list_raw
+
+    for alert in alerts_list:
+        alert["longitude"] = alert["location"]["x"]
+        alert["latitude"] = alert["location"]["y"]
+        alert.pop("location", None)
+
+    return alerts_list
+
+
+def prepare_payloads(alerts_list):
+    """Summary
+    
+    Args:
+        alerts_list (TYPE): Description
+    """
+    alerts_df = pd.DataFrame.from_dict(alerts_list)
+
+
+def post_to_postgre(alerts_list):
+    """Summary
+    
+    Args:
+        alerts_list (TYPE): Description
+    
+    Returns:
+        TYPE: Description
+    """
+
+    counter = 0
+
+    for index, alert in enumerate(alerts_list):
+        alerts_list[index] = dict((k.lower(), v) for k, v in alert.items())
+
+    for alert in alerts_list:
+
+        try:
+            pgrest.upsert(alert)
+            counter+=1
+
+
+        except:
+            return pgrest.res.text
+
+    return alerts_list
+
+def main():
+    """Summary
+    
+    Returns:
+        TYPE: Description
+    """
+    alerts_list_raw = scrape_waze_record()
+    alerts_list = split_coord(alerts_list_raw)
+
+    res = post_to_postgre(alerts_list)
+
+    return len(res)
+
+
+if __name__ == "__main__":
+
+    print(main())
+
